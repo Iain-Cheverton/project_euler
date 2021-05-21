dc258e06713416b358948d7ed73e245bee026370
Iain <lich294@gmail.com>
2021-05-21 16:23:12 +0100

    Added first pytest test

--- problem_1.py
+++ problem_1.py
@@ -1,6 +1,3 @@
-import pytest
-
-
 def multiple_sum(upper_bound):
     total = 0
     for i in range(upper_bound):
@@ -9,11 +6,6 @@ def multiple_sum(upper_bound):
         elif i % 5 == 0:
             total += i
     return total
-
-
-if multiple_sum(10) != 23:
-    print("sum is not working correctly")
-    exit()
 
 
 print(multiple_sum(1000))
---
+++ test_problem_1.py
@@ -0,0 +1,5 @@
+import problem_1
+
+
+def test_multiple_sum():
+    assert problem_1.multiple_sum(10) == 23
