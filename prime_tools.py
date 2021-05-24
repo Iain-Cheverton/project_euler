7492be1f10700926b144e37ce1700ed778c9616d
Iain <lich294@gmail.com>
2021-05-24 18:38:36 +0100

    Solved problem 3

---
+++ prime_tools.py
@@ -0,0 +1,27 @@
+import math
+
+
+def find_primes(bound):
+    primes = [2]
+    temp_primes = [2]
+    for i in range(2, bound):
+        root = math.sqrt(i)
+        for prime in primes:
+            if i % prime == 0:
+                break
+            if prime > root:
+                temp_primes.append(i)
+                break
+        primes = temp_primes
+
+    return primes
+
+
+def prime_factors(integer):
+    primes = find_primes(int(math.sqrt(integer)))
+    factors = []
+    for prime in primes:
+        if integer % prime == 0:
+            factors.append(prime)
+
+    return factors
---
+++ problem_3.py
@@ -0,0 +1,14 @@
+"""
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+"""
+
+import time
+
+import prime_tools
+
+start_time = time.perf_counter()
+
+print(prime_tools.prime_factors(600851475143))
+print(time.perf_counter() - start_time)
