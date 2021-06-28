c94d661b3c6de5061fd3e4a9a8b17823dda8bc22
Iain Cheverton <lich294@gmail.com>
2021-06-28 11:20:17 +0100

    Solved problem 36

---
+++ problem_36.py
@@ -0,0 +1,18 @@
+"""
+The decimal number, 585 = 1001001001(binary), is palindromic in both bases.
+Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+(Please note that the palindromic number, in either base, may not include leading zeros.)
+"""
+
+
+def main():
+    """returns a list of all numbers under 1000000 that are palindromic in both base 10 and 2"""
+    double_palindromes = []
+    for integer in range(1_000_000):
+        if str(integer) == str(integer)[::-1] and bin(integer)[2:] == bin(integer)[:1:-1]:
+            double_palindromes.append(integer)
+    return double_palindromes
+
+
+if __name__ == "__main__":
+    print(sum(main()))
