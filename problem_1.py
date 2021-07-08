e9e63a8ca9d26abc99e3244994a80bee89a5495c
Iain Cheverton <lich294@gmail.com>
2021-07-08 16:57:52 +0100

    rewrote problem 1

--- problem_1.py
+++ problem_1.py
@@ -6,13 +6,7 @@ Find the sum of all the multiples of 3 or 5 below 1000."""
 
 def main(upper_bound):
     """returns the sum of the numbers that divide by 3 or 5 below 1000"""
-    total = 0
-    for i in range(upper_bound):
-        if i % 3 == 0:
-            total += i
-        elif i % 5 == 0:
-            total += i
-    return total
+    return sum([i for i in range(upper_bound) if not (i % 3 and i % 5)])
 
 
 if __name__ == "__main__":
